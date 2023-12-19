2ae8f148f299e10c351ea4d8e067c0febe9d85f9
Gannicus <gannicus@orbitus.ca>
2023-12-19 17:13:59 -0500

    Iindexing

---
+++ src/app/pages/index.ts
@@ -0,0 +1,7 @@
+export * from './dev';
+
+export * from './welcome/welcome.component';
+
+export * from './featured/featured.component';
+export * from './mod/mod.component';
+export * from './modpack/modpack.component';
