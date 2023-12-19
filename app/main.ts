f36da5f85afb9735fb7f8f19b90c0980d2d9707b
Gannicus <gannicus@orbitus.ca>
2023-12-19 17:09:25 -0500

    Update our main electron file

--- app/main.ts
+++ app/main.ts
@@ -1,26 +1,134 @@
-import {app, BrowserWindow, screen} from 'electron';
+import { app, BrowserWindow, screen } from 'electron';
+import { autoUpdater, UpdateDownloadedEvent } from 'electron-updater';
+import { Client } from 'discord-rpc';
+
 import * as path from 'path';
 import * as fs from 'fs';
 
 let win: BrowserWindow | null = null;
+let client: Client | null = null;
+let startTimestamp: Date = new Date();
+let discordRetryDuration: number = 15;
 const args = process.argv.slice(1),
-  serve = args.some(val => val === '--serve');
+  serve = args.some((val) => val === '--serve');
+
+function connectDiscord(): any {
+  if (client) {
+    client.destroy();
+  }
+
+  client = new Client({
+    transport: 'ipc',
+  });
+
+  client.on('ready', async () => {
+    console.debug(
+      `Successfully authorised as ${client?.user?.username}#${client?.user?.discriminator}`
+    );
+    onStartup();
+  });
+
+  client.once('close', () => {
+    console.error(`Connection to Discord closed. Attempting to reconnect...`);
+    console.debug(
+      `Automatically retrying to connect, please wait ${discordRetryDuration} seconds...`
+    );
+    connectDiscord();
+  });
+
+  setTimeout(() => {
+    client?.login({ clientId: '1186462656081698846' });
+  }, discordRetryDuration * 1000);
+}
+
+function updatePresence(): any {
+  console.log(
+    `Successfully updated ${client?.user?.username}#${client?.user?.discriminator}'s Rich Presence!`
+  );
+
+  return client?.setActivity({
+    details: 'Modifying my mods',
+    largeImageKey: 'icon',
+    largeImageText: 'Lethalize Manager',
+    smallImageKey: 'featured',
+    smallImageText: 'Made by @FearGanni',
+    buttons: [
+      {
+        label: 'Check the github',
+        url: 'https://github.com/fearganni/LethalizeManager',
+      },
+    ],
+    startTimestamp: startTimestamp,
+    instance: true,
+  });
+}
+
+let initialTasks = [updatePresence],
+  i = 0;
+
+const onStartup = () => {
+  initialTasks[i++]();
+  if (i < initialTasks.length) {
+    setTimeout(onStartup, 5 * 1000); // 5 seconds
+  }
+};
+
+// Check for application updates
+function checkUpdates(): void {
+  autoUpdater.checkForUpdatesAndNotify();
+
+  autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for update...');
+  });
+  autoUpdater.on('update-available', (info) => {
+    console.log('Update available.');
+  });
+  autoUpdater.on('update-not-available', (info) => {
+    console.log('Update not available.');
+  });
+  autoUpdater.on('error', (err) => {
+    console.log('Error in auto-updater. ' + err);
+  });
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message =
+      log_message +
+      ' (' +
+      progressObj.transferred +
+      '/' +
+      progressObj.total +
+      ')';
+
+    win?.setProgressBar(progressObj.percent / 100);
+
+    console.log(log_message);
+  });
+  autoUpdater.on('update-downloaded', (ev: UpdateDownloadedEvent) => {
+    console.log('Update downloaded');
+
+    setTimeout(function () {
+      autoUpdater.quitAndInstall();
+    }, 5000);
+  });
+}
 
 function createWindow(): BrowserWindow {
-
   const size = screen.getPrimaryDisplay().workAreaSize;
 
   // Create the browser window.
   win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height,
+    center: true,
+    width: size.width / 1.5,
+    height: size.height / 1.5,
     webPreferences: {
       nodeIntegration: true,
-      allowRunningInsecureContent: (serve),
+      allowRunningInsecureContent: serve,
       contextIsolation: false,
     },
+
+    autoHideMenuBar: true,
+    backgroundMaterial: 'mica',
   });
 
   if (serve) {
@@ -34,7 +142,7 @@ function createWindow(): BrowserWindow {
     let pathIndex = './index.html';
 
     if (fs.existsSync(path.join(__dirname, '../dist/index.html'))) {
-       // Path when running electron in local folder
+      // Path when running electron in local folder
       pathIndex = '../dist/index.html';
     }
 
@@ -58,7 +166,11 @@ try {
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
   // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-  app.on('ready', () => setTimeout(createWindow, 400));
+  app.on('ready', () => {
+    checkUpdates();
+    connectDiscord();
+    setTimeout(createWindow, 400);
+  });
 
   // Quit when all windows are closed.
   app.on('window-all-closed', () => {
@@ -76,7 +188,6 @@ try {
       createWindow();
     }
   });
-
 } catch (e) {
   // Catch Error
   // throw e;
