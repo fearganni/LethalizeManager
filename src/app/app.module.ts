ec52ad8da882c825831ba3af512cd7789245cb5d
Gannicus <gannicus@orbitus.ca>
2023-12-20 19:57:55 -0500

    Add all our new files into app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
@@ -11,34 +11,93 @@ import { AppRoutingModule } from './app-routing.module';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { HomeModule } from './home/home.module';
-import { DetailModule } from './detail/detail.module';
+// NgxBootstrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// NgxToastr
+import { ToastrModule } from 'ngx-toastr';
+
+// NgxMarkdown
+import { MarkdownModule } from 'ngx-markdown';
+
+// NgxMoment
+import { MomentModule } from 'ngx-moment';
+
+// NgSelect
+import { NgSelectModule } from '@ng-select/ng-select';
 
 import { AppComponent } from './app.component';
 
+// Components
+import {
+  FooterComponent,
+  HeaderComponent,
+  SidebarComponent,
+} from './shared/components';
+
+// Pages
+import {
+  UpdatesComponent,
+  UpdateComponent,
+  CreditComponent,
+  WelcomeComponent,
+  FeaturedComponent,
+  ModComponent,
+  ModpackComponent,
+} from './pages';
+
 // AoT requires an exported function for factories
-const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>  new TranslateHttpLoader(http, './assets/i18n/', '.json');
+const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>
+  new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+
+    // Components
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+
+    // Pages
+    UpdatesComponent,
+    UpdateComponent,
+    CreditComponent,
+
+    WelcomeComponent,
+    FeaturedComponent,
+    ModpackComponent,
+    ModComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     CoreModule,
     SharedModule,
-    HomeModule,
-    DetailModule,
     AppRoutingModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
         useFactory: httpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })
+        deps: [HttpClient],
+      },
+    }),
+    NgbModule,
+    ToastrModule.forRoot({
+      timeOut: 15000,
+      extendedTimeOut: 2000,
+      positionClass: 'toast-bottom-center',
+      preventDuplicates: true,
+      progressBar: true,
+    }),
+    MarkdownModule.forRoot(),
+    MomentModule,
+    NgSelectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
