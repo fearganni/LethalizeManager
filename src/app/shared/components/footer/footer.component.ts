25ef4579924b85f98594a5e8be290dd79e90d6a9
Gannicus <gannicus@orbitus.ca>
2023-12-20 19:56:37 -0500

    Add footer design

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ElectronService } from '../../../core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styles: [],
+})
+export class FooterComponent implements OnInit {
+  appVersion: string = '0.0.0';
+
+  constructor(private electronService: ElectronService) {}
+
+  ngOnInit(): void {
+    this.appVersion = this.electronService.getVersion;
+  }
+}
