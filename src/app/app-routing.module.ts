a4e4d8879bc36235b3cb786dd1f96850156b8ce8
Gannicus <gannicus@orbitus.ca>
2023-12-20 19:58:14 -0500

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,57 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './shared/components';
-
-import { HomeRoutingModule } from './home/home-routing.module';
-import { DetailRoutingModule } from './detail/detail-routing.module';
+import {
+  UpdatesComponent,
+  UpdateComponent,
+  ModComponent,
+  ModpackComponent,
+  WelcomeComponent,
+} from './pages';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: WelcomeComponent,
+    data: {
+      title: 'Home',
+    },
+  },
+  {
+    path: 'development',
+    component: UpdatesComponent,
+    data: {
+      title: 'Developer Updates',
+    },
+  },
+
+  {
+    path: 'mods',
+    component: ModComponent,
+    data: {
+      title: 'Mods',
+    },
+  },
+  {
+    path: 'pack',
+    component: ModpackComponent,
+    data: {
+      title: 'Pack',
+    },
   },
   {
     path: '**',
-    component: PageNotFoundComponent
-  }
+    component: PageNotFoundComponent,
+  },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {}),
-    HomeRoutingModule,
-    DetailRoutingModule
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {})],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
