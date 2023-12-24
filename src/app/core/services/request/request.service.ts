a7fa436e529ad73c16616c35f806f907aa5cf6c2
Feargannicus <gannicus@exordium.org>
2023-12-24 15:21:53 -0500

    Update request service to pass linting

--- src/app/core/services/request/request.service.ts
+++ src/app/core/services/request/request.service.ts
@@ -4,8 +4,14 @@ import {
   HttpHeaders,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
 import { Observable, catchError, throwError } from 'rxjs';
+
+import { ToastrService } from 'ngx-toastr';
+
+interface SuccessResponse {
+  statusCode: number;
+  message: string;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -23,14 +29,15 @@ export class RequestService {
   constructor(private http: HttpClient, private ts: ToastrService) {}
 
   // Error handler
-  errorHandle(e: HttpErrorResponse) {
-    let message: any;
+  private errorHandle = (e: HttpErrorResponse): Observable<never> => {
+    let message = '';
 
-    if (typeof e.error.errors !== 'undefined') {
-      message = `${e.error.errors[0].msg} for field ${e.error.errors[0].param}`;
-    } else if (typeof e.error.message !== 'undefined') {
+    if (e.error && 'errors' in e.error && e.error.errors.length) {
+      const firstError = e.error.errors[0] as { msg: string; param: string }; // Assuming a structure for the error
+      message = `${firstError.msg} for field ${firstError.param}`;
+    } else if (e.error && 'message' in e.error) {
       message = e.error.message;
-    } else if (typeof e.message !== undefined) {
+    } else if (e.message) {
       message = e.message;
     } else {
       message =
@@ -38,38 +45,44 @@ export class RequestService {
     }
 
     return throwError(() => new Error(message));
-  }
+  };
 
   // Handle errors
-  errorText(e: any): string {
-    let message: string = '';
-    if (typeof e.errors !== 'undefined') {
+  private errorText = (e: any): string => {
+    let message: string;
+
+    if (e.errors?.length) {
       message = e.errors[0].msg;
-    } else if (typeof e.message !== 'undefined') {
+    } else if (e.message) {
       message = e.message;
     } else {
       message =
         'An unknown error has occurred. Please let our support team know if this issue continues!';
     }
+
     return message;
-  }
-  error(e: any): void {
-    this.ts.error(this.errorText(e));
-  }
+  };
+
+  error = (e: unknown): void => {
+    this.ts.error(this.errorText(e as any)); // Casting to 'any' to avoid type error; make sure 'e' is an appropriate error object
+  };
 
   // Handle successes
-  success(s: any, title?: string): void {
+  success = (s: SuccessResponse, title?: string): void => {
     if (s.statusCode >= 200 && s.statusCode <= 299) {
-      this.ts.success(s.message, title);
+      this.ts.success(s.message, title || ''); // Ensure title is a string
     } else {
-      this.ts.warning(s.message, title);
+      this.ts.warning(s.message, title || ''); // Ensure title is a string
     }
-  }
+  };
 
   // Get request
-  get<T>(url: string): Observable<T> {
+  get<T>(url: string, responseType?: string): Observable<T> {
     return this.http
-      .get<T>(url, { headers: this.headers })
+      .get<T>(url, {
+        headers: this.headers,
+        responseType: (responseType as 'json') || 'json',
+      })
       .pipe(catchError(this.errorHandle));
   }
 
@@ -78,7 +91,6 @@ export class RequestService {
     return this.http
       .post<T>(url, data, {
         headers: this.headers,
-        responseType: 'json',
       })
       .pipe(catchError(this.errorHandle));
   }
@@ -86,7 +98,7 @@ export class RequestService {
   // Patch request
   patch<T>(data: any, url: string): Observable<T> {
     return this.http
-      .patch<T>(url, data, { headers: this.headers, responseType: 'json' })
+      .patch<T>(url, data, { headers: this.headers })
       .pipe(catchError(this.errorHandle));
   }
 
@@ -95,7 +107,6 @@ export class RequestService {
       .delete<T>(url, {
         body: data,
         headers: this.headers,
-        responseType: 'json',
       })
       .pipe(catchError(this.errorHandle));
   }
