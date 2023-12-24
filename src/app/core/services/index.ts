8115e7808d78374de13187421568dd4257756274
Feargannicus <gannicus@exordium.org>
2023-12-24 15:22:22 -0500

    Indexing

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,3 +1,4 @@
 export * from './electron/electron.service';
 export * from './jarallax/jarallax.service';
 export * from './request/request.service';
+export * from './thunderstore/thunderstore.service';
