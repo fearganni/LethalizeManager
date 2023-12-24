5c4bc68229cd8d7ca31c52007b8b210a95bcd99e
Feargannicus <gannicus@exordium.org>
2023-12-24 15:25:35 -0500

    Update page not found

--- src/app/shared/components/page-not-found/page-not-found.component.ts
+++ src/app/shared/components/page-not-found/page-not-found.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { AppSettings } from '../../../core';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: ['./page-not-found.component.scss'],
 })
-export class PageNotFoundComponent implements OnInit {
-  constructor() {}
+export class PageNotFoundComponent implements OnInit, OnDestroy {
+  AppSettings = AppSettings;
+
+  swiperGradient = '89, 20, 20';
+  swiperBG = `linear-gradient(180deg, rgba(${this.swiperGradient}, 0) 0%, rgba(${this.swiperGradient}, 0.8) 75%, rgba(${this.swiperGradient}, 0.9) 100%)`;
+
+  constructor() {
+    this.AppSettings.appEmpty = true;
+  }
 
   ngOnInit(): void {
     console.log('PageNotFoundComponent INIT');
+  }
+
+  ngOnDestroy(): void {
+    this.AppSettings.appEmpty = false;
   }
 }
